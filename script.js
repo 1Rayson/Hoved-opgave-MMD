f3b4d582716661b0c2730e023ec42f438d31b3d5
1Rayson <andersskrydstrup@gmail.com>
2022-05-29 14:55:06 +0200

    edited styling and attempts at fixing the script

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
+// function myFunction() {
+//     // Declare variables
+//     let input, filter, ul, li, a, i, txtValue;
+//     input = document.getElementById('myInput');
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("myUL");
+//     li = ul.getElementsByTagName('li');
+
+//     // Loop through all list items, and hide those who don't match the search query
+//     for (i = 0; i < li.length; i++) {
+//         a = li[i].getElementsByTagName("a")[0];
+//         txtValue = a.textContent || a.innerText;
+//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//             li[i].style.display = "";
+//         } else {
+//             li[i].style.display = "none";
+//         }
+//     }
+// }
+
 function myFunction() {
-    // Declare variables
-    let input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
+    let filters = [myInput];
 
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
+
+    const tagsElements = document.querySelectorAll('div,iframe,li');
+    console.log(tagsElements);
+
+    // loop through all elements and find matching tags
+    tagsElements.forEach(element => {
+        let tags = element.dataset.tags.split(',');
+        // check if the set of tags includes any of the terms in set in the filters
+        if (tags.some(r => filters.includes(r))) {
+            console.log('found!');
+            element.classList.add('active');
         }
-    }
+        console.log(tags.some(r => filters.includes(r)));
+        console.log(tags);
+    });
 }
-
-
-// let filters = ['cars',];
-
-
-// const tagsElements = document.querySelectorAll('div');
-// console.log(tagsElements);
-
-// // loop through all elements and find matching tags
-// tagsElements.forEach(element => {
-//   let tags = element.dataset.tags.split(',');
-//   // check if the set of tags includes any of the terms in set in the filters
-//   if(tags.some(r=> filters.includes(r))){
-//     console.log('found!');
-//     element.classList.add('active');
-//   }
-//   console.log(tags.some(r=> filters.includes(r)));
-//   console.log(tags);
-// });
