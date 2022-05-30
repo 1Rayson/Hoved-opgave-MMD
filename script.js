5fd5f0eecc2a8cdf65c3f987b1dda217503688d9
1Rayson <andersskrydstrup@gmail.com>
2022-05-30 14:01:48 +0200

    script now works
    styling and javascript is being added
    HTML part is done

--- script.js
+++ script.js
@@ -1,39 +1,63 @@
-// function myFunction() {
-//     // Declare variables
-//     let input, filter, ul, li, a, i, txtValue;
-//     input = document.getElementById('myInput');
-//     filter = input.value.toUpperCase();
-//     ul = document.getElementById("myUL");
-//     li = ul.getElementsByTagName('li');
+const tagListElement = document.querySelector('.tagList');
+const elementsForFiltering = document.querySelectorAll('div');
+const tagList = [
+    // whenever new videos are made add tags as required
+    "sensor",
+    "install",
+    "how to",
+    "app",
+];
 
-//     // Loop through all list items, and hide those who don't match the search query
-//     for (i = 0; i < li.length; i++) {
-//         a = li[i].getElementsByTagName("a")[0];
-//         txtValue = a.textContent || a.innerText;
-//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//             li[i].style.display = "";
-//         } else {
-//             li[i].style.display = "none";
-//         }
-//     }
-// }
+function generateTagList() {
+    // loop through the tag list
+    tagList.forEach(tag => {
+        // generate HTML elements
+        let li = document.createElement("li");
+        // create and append text node with the tag value
+        li.appendChild(document.createTextNode(tag));
+        // add class
+        li.classList.add('tagList__tag');
 
-function myFunction() {
-    let filters = [myInput];
+        // add eventlisteners  
+        li.addEventListener('click', () => {
+            console.log(li.textContent);
+            filterTags(li.textContent);
+        });
 
+        // add to list
+        tagListElement.appendChild(li);
 
-    const tagsElements = document.querySelectorAll('div,iframe,li');
-    console.log(tagsElements);
+    });
 
-    // loop through all elements and find matching tags
-    tagsElements.forEach(element => {
+}
+
+function filterTags(tag) {
+    console.log('filtering for tag: ', tag);
+
+    elementsForFiltering.forEach(element => {
         let tags = element.dataset.tags.split(',');
         // check if the set of tags includes any of the terms in set in the filters
-        if (tags.some(r => filters.includes(r))) {
+        console.log(tags);
+        if (tags.some(r => [tag].includes(r))) {
             console.log('found!');
             element.classList.add('active');
+            element.classList.remove('inactive');
+        } else {
+            element.classList.remove('active');
+            element.classList.add('inactive');
         }
-        console.log(tags.some(r => filters.includes(r)));
+        console.log(tags.some(r => tagList.includes(r)));
         console.log(tags);
     });
+
 }
+
+function resetAllFilters() {
+    elementsForFiltering.forEach(element => {
+        element.classList.remove('active');
+        element.classList.remove('inactive');
+    });
+}
+generateTagList();
+
+document.querySelector('.resetFilters').addEventListener('click', resetAllFilters);
